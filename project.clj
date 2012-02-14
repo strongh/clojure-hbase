1b888c041ced22b0f72e4b8584d6492067a95926
Dan Lidral-Porter <dan@luckysort.com>
2012-02-13 17:34:55 -0800

    Use CDH3u3 hadoop & hbase jars.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject org.clojars.strongh/clojure-hbase "0.90.5"
+(defproject org.clojars.strongh/clojure-hbase "0.90.4-cdh3u3"
   :description "A convenient Clojure interface to HBase."
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.apache.hadoop/hadoop-core "0.20.2-dev"]
-                 [org.apache.hbase/hbase "0.90.5"]
+                 [org.clojars.aperiodic/hadoop "0.20.2-cdh3u3-core"]
+                 [org.clojars.aperiodic/clojure-hbase "0.90.4-cdh3u3"]
                  [org.apache.zookeeper/zookeeper "3.3.3"]
                  [log4j/log4j "1.2.16"]
                  [commons-logging/commons-logging "1.0.4"]])
